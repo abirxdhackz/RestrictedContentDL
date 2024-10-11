76301bab2b303748892b49a9afffdbe2d12137c8
Abir Arafat Chawdhury <abirthedigitalguru@gmail.com>
2024-10-11 20:49:45 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# Copyright (C) @abirxdhackz
+# Channel: https://t.me/abir_xd_bio
+
+# Pyrogram setup
+API_ID = "
+22179294"  # Replace this API ID with your actual API ID
+API_HASH = "02a7d59472246972cb827ce1d8e1e2e2"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8063825557:AAEUZTCcvVv9F7LNYSXrUOKZ1iTf4rgR_jE"  # Replace this BOT_TOKEN
+SESSION_STRING = "BQFSbd4AF1_EIgOawHRKWaq_nZDqQtbWkcUnwkmyCLNcDaQveqdEhS5tsjMmZqCXLjnrMUaG-qHGsiq-ahAyZWinWQUoyAUaeF61shyrqV_VUWArlGd0W7ul4sRRyNFYW9tTjKEiTC_gDrxUl2hYih5LFgJhw9F_vsrAzR5K5A4v9tHnT7HvWqEDG0Y4YoPE02SqRt7UJ3BSRTmvdkFbwxZBC3MIR_XxIcYQwnornn8wQr-Y94qjvJm9I_dVHQ0aB7XAt8dKPX7-RNpyu2h5L3x239OLER0qgf-qXhbOKoxTfGejjewYLJ0umYYkqu4yfjWgROCxl1wa6m8jDY2U3hVNnYuX5QAAAAGzV09gAA" # Replace this SESSION STRING with your actual SESSION_STRING
